a44d275fd4553bbb63114cfc32398ea54e20dd40
Nikita <wolf24ru@gmail.com>
2022-01-25 11:29:37 +0700

    change env variables

--- stocks_products/settings.py
+++ stocks_products/settings.py
@@ -21,10 +21,12 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY', 'super-secret-key')
+# SECRET_KEY = os.getenv('SECRET_KEY', 'super-secret-key')
+SECRET_KEY = 'sdfhjo8sdufiu8sduyfj'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = bool(os.getenv('DEBUG'))
+# DEBUG = bool(os.getenv('DEBUG'))
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
