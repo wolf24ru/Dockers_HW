b968047052c1ca7954859722a1fe6ee65088b8ba
Nikita <wolf24ru@gmail.com>
2022-01-26 15:51:48 +0700

    new commit and try run test #2

--- tests/test_create_account.py
+++ tests/test_create_account.py
@@ -4,17 +4,17 @@ import pytest
 class TestAccount:
 
     @pytest.mark.django_db(transaction=True)
-    def test_create_user(self, django_user):
-        user = django_user.objects.create_user(username='user',
+    def test_create_user(self, django_user_model):
+        user = django_user_model.objects.create_user(username='user',
                                                email='user@user.com',
                                                password='user',
                                                )
         assert user.is_active, 'user not active'
 
     @pytest.mark.django_db(transaction=True)
-    def test_email(self, django_user):
+    def test_email(self, django_user_model):
         with pytest.raises(ValueError):
-            assert django_user.objects.create_user(username='user',
+            assert django_user_model.objects.create_user(username='user',
                                                    password='user'
                                                    ), 'Email is not required'
 
